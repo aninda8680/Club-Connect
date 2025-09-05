ae296386ac35aca170367a1fcdbce04e0fc73aa5
aninda8680 <anindadebta8680@gmail.com>
2025-09-06 02:15:15 +0530

    updated

--- FRONTEND/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import type { AxiosInstance } from "axios";
-
-// Explicitly type API as AxiosInstance
-const API: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL as string, // 👈 tell TS it's a string
-});
-
-export default API;
