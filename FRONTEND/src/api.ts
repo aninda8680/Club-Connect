98009d1f46ef39d71a2758f4cc76ea395172d799
aninda8680 <anindadebta8680@gmail.com>
2025-10-13 00:45:42 +0530

    updated

--- FRONTEND/src/api.ts
+++ FRONTEND/src/api.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ||
-  (import.meta.env.DEV
-    ? "http://localhost:5000/api"
-    : "https://club-connect-p2o2.onrender.com/api");
+  import.meta.env.VITE_API_BASE_URL 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
