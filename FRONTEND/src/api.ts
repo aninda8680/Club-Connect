be0233a550d8aa9644f74640526f83c9f7e5f853
aninda8680 <anindadebta8680@gmail.com>
2025-10-13 01:06:11 +0530

    updated

--- FRONTEND/src/api.ts
+++ FRONTEND/src/api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL 
-  || "http://localhost:5000/api";
-console.log("✅ Using API base URL:", import.meta.env.VITE_API_BASE_URL);
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+
+console.log("✅ API Base URL:", API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: false,
 });
+
 
 export default api;
